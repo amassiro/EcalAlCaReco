fab3744ebbd41687f6c70588c7aae911c0639b6a
amassiro <massironi.andrea@gmail.com>
2018-06-16 20:19:36 +0200

    up

--- step21_RAW2DIGI_RECO_EI_ALCA.py
+++ step21_RAW2DIGI_RECO_EI_ALCA.py
@@ -7,7 +7,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process('RECO',eras.Run2_2018)
+process = cms.Process('MYRECO',eras.Run2_2018)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
